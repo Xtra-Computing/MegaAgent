0c3549e08c0c69b0b4fffc6c62228f710545d661
Tianyu Wang <wty500@sjtu.edu.cn>
2024-08-10 23:45:24 +0800

    init

---
+++ Gobang/config.py
@@ -0,0 +1,31 @@
+api_key = 'Your key here'
+model = "gpt-4o"
+url = 'https://api.openai.com/v1/chat/completions'
+
+MAX_LEN = 6
+ceo_name = "Bob"
+initial_prompt = r'''
+You are Bob, the leader of a software development club. Your club's current goal is to develop a Gobang game with a very strong AI, no frontend, and can be executed by running 'main.py'. You are now recruiting employees and assigning work to them. For each employee(including yourself), please write a prompt. Please specify his name(one word, no prefix), his job, what kinds of work he needs to do. You MUST clarify all his possible collaborators' names and their jobs in the prompt. The format should be like (The example is for Alice in another novel writing project):
+
+<employee name="Alice">
+You are Alice, a novelist. Your job is to write a single chapter of a novel with 1000 words according to the outline (outline.txt) from Carol, the architect designer, and pass it to David (chapter_x.txt), the editor. Please only follow this routine. Your collarborators include Bob(the Boss), Carol(the architect designer) and David(the editor).
+</employee>
+
+Please note that every employee is lazy, and will not care anything not mentioned by your prompt. To ensure the completion of your project, the work of each employee should be **non-divisable**, detailed in specific action(like what file to write. Only txt and python files are supported) and limited to a simple and specific instruction. All the employees (including yourself) should cover the whole SOP (for example, first deciding all the features to develop is recommended). Speed up the process by adding more employees to divide the work.
+
+After the designation process, please specify an employee's name to initiate the whole project, in the format <beginner>Name</beginner>.
+'''
+
+additional_prompt = r'''
+Your club's current goal is to develop a Gobang game with a very strong AI, no frontend, and can by executed by running 'main.py'. The project should be executable in files.
+
+You MUST use exactly <talk goal="Name">TalkContent</talk> format to talk to others, like:
+
+<talk goal="Alice">Alice, I have completed 'a.txt'. Please check it for your work and talk to me again if needed. </talk>. 
+
+Otherwise, they will not receive your message, and the conversation will terminate. "Name" should only be ONE specific employee. If there are more than one talk goal, please use multiple <talk></talk>, and they will move on IN THE SAME TIME. 
+
+You must use function calls in JSON for file operations.
+
+Leave a remarkable TODO wherever there is an unfinished task. Please keep updating your TODO list until everything is done. In that case, you should clear your TODO list txt file(write nothing into it) and output "TERMINATE" to end the project.
+'''
